3da5dddca9385b4f7b88978664678489ed0adf4b
Lev <Chorolop@163.com>
2024-05-08 13:01:45 +0800

    完善Job和遥控模式

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -84,8 +84,8 @@ extern u8 PID_Send;
 extern float Acceleration_Z;                       //Z轴加速度计
 extern float Balance_Kp,Balance_Kd,Velocity_Kp,Velocity_Ki,Turn_Kp,Turn_Kd;
 
-extern int job_dir[4][3];										// GUA: 添加外部变量声明，方便蓝牙修改
-extern int job_counter, job_action, job_pointer;					// 这里是方便show.c里面展示数据
+extern int job_dir[4][3], job_list[60];								// GUA: 添加外部变量声明，方便蓝牙修改
+extern int job_counter, job_action, job_pointer, job_usart_pointer;	// 这里是方便show.c里面展示数据
 
 extern float Velocity_Left,Velocity_Right;	//车轮速度(mm/s)
 //外部声明当前速度
